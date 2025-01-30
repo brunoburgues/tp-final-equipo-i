73d930bf3e9cae53748f54da0d4699dfa853828c
Pedrín-ELCOMPUTADOR2 <brunoburguesadf@gmail.com>
2025-01-30 18:20:29 -0300

    tabla script

--- TPfinal_equipo_I/TPfinal_equipo_I/Agregar.aspx.designer.cs
+++ TPfinal_equipo_I/TPfinal_equipo_I/Agregar.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace TPfinal_equipo_I
         protected global::System.Web.UI.WebControls.TextBox txtNombre;
 
         /// <summary>
+        /// Control txtPrecio.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPrecio;
+
+        /// <summary>
         /// Control txtStock.
         /// </summary>
         /// <remarks>
@@ -58,6 +67,15 @@ namespace TPfinal_equipo_I
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtImagen;
+
+        /// <summary>
+        /// Control txtDescripcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtDescripcion;
 
         /// <summary>
         /// Control alertaMensaje.
